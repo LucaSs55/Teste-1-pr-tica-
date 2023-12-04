d8b046447482235efca60b5edc7f29d9d8953ec6
Lucas-Santos <lucassantos2018d@gmail.com>
2023-12-04 13:32:16 -0300

    commit inicial

---
+++ Teste-00.py
@@ -0,0 +1,4 @@
+Dia= input("Digite seu dia de nascimento")
+mes= input("Digite seu mês de nascimento")
+ano= input("Digite seu ano de nascimento")
+print("Você nasceu no", dia, "de", mes,"de", ano,"correto?")
